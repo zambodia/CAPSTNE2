0ef4141184e9551adb1c37a9630902605b584e36
zambodia <zambodia222@gmail.com>
2015-02-01 16:57:11 +0800

    TestScript3 - YOYOYO

--- Mattsample/Assets/Scripts/TestScript3.cs
+++ Mattsample/Assets/Scripts/TestScript3.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class TestScript3 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	//YOYOYO
 	}
 	
 	// Update is called once per frame
